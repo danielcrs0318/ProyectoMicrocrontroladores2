31f70d2b8d01e683eac652a3d077874e2da7e066
danielcrs0318 <118858545+danielcrs0318@users.noreply.github.com>
2024-07-18 21:26:53 -0600

    iniciar la conexion de la arduino al iniciar el menu

--- ProyectoMicrocrontroladores2/MenuPrincipal.cs
+++ ProyectoMicrocrontroladores2/MenuPrincipal.cs
@@ -1,4 +1,5 @@
-﻿using ProyectoMicrocrontroladores2.formularios;
+﻿using ProyectoMicrocrontroladores2.Clases;
+using ProyectoMicrocrontroladores2.formularios;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -12,10 +13,14 @@ using System.Windows.Forms;
 namespace ProyectoMicrocrontroladores2
 {
     public partial class MenuPrincipal : Form
+
     {
+        private ConexionArduino arduinoConnection;
         public MenuPrincipal()
         {
             InitializeComponent();
+            string cadenaConexion = "Server=localhost;Database=NombreBaseDatos;User Id=Usuario;Password=Contraseña;";
+            arduinoConnection = new ConexionArduino("COM3", 9600, cadenaConexion); // Cambia "COM3" por el puerto que esté usando tu Arduino
         }
 
         private void sENSORHUMEDADDESUELOToolStripMenuItem_Click(object sender, EventArgs e)
@@ -30,6 +35,15 @@ namespace ProyectoMicrocrontroladores2
             frmSensorHumedadSuelo frmsensorhumedad = new frmSensorHumedadSuelo();
             frmsensorhumedad.MdiParent = this;
             frmsensorhumedad.Show();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            arduinoConnection.IniciarConexion();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            arduinoConnection.CerrarConexion();
         }
     }
 }
