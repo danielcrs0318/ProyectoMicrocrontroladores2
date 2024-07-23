85d7a50b7b48a5806c35327e8cf6e05daa259f33
danielcrs0318 <118858545+danielcrs0318@users.noreply.github.com>
2024-07-23 16:45:39 -0600

    cambios en formulario sensor de humedad con el elemento chart

--- ProyectoMicrocrontroladores2/formularios/frmSensorHumedadSuelo.Designer.cs
+++ ProyectoMicrocrontroladores2/formularios/frmSensorHumedadSuelo.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSensorHumedadSuelo));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -38,8 +42,10 @@
             this.BTNCONECTAR = new System.Windows.Forms.Button();
             this.BTNDESCONECTAR = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHumedadSuelo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -86,7 +92,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Red;
             this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.Location = new System.Drawing.Point(426, 1);
+            this.button1.Location = new System.Drawing.Point(823, 1);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(30, 32);
             this.button1.TabIndex = 6;
@@ -135,12 +141,36 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Silver;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(466, 87);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.LabelBackColor = System.Drawing.Color.Silver;
+            series1.Legend = "Legend1";
+            series1.Name = "Temperatura";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(376, 391);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            title1.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.Name = "Title1";
+            title1.Text = "Datos Sensor Humedad del Suelo";
+            this.chart1.Titles.Add(title1);
+            // 
             // frmSensorHumedadSuelo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(455, 492);
+            this.ClientSize = new System.Drawing.Size(855, 492);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.BTNDESCONECTAR);
             this.Controls.Add(this.BTNCONECTAR);
@@ -156,6 +186,7 @@
             this.Load += new System.EventHandler(this.frmSensorHumedadSuelo_Load);
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvHumedadSuelo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,5 +203,6 @@
         private System.Windows.Forms.Button BTNCONECTAR;
         private System.Windows.Forms.Button BTNDESCONECTAR;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
