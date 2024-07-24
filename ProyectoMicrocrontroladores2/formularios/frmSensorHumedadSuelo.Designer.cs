dad6f064cf1fc038fe233c2f340ad7ac13f44731
danielcrs0318 <118858545+danielcrs0318@users.noreply.github.com>
2024-07-23 20:47:01 -0600

    cambios de intwerfaz formulario sensor de humedad de suelo

--- ProyectoMicrocrontroladores2/formularios/frmSensorHumedadSuelo.Designer.cs
+++ ProyectoMicrocrontroladores2/formularios/frmSensorHumedadSuelo.Designer.cs
@@ -51,10 +51,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(55, 43);
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(235, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(340, 21);
+            this.label1.Size = new System.Drawing.Size(377, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "SENSOR DE HUMEDAD DE SUELO";
             // 
