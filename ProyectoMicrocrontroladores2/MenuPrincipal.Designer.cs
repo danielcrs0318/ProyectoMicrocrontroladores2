0cfa06b82436fa61c26b76360ce2d060301b9e04
danielcrs0318 <118858545+danielcrs0318@users.noreply.github.com>
2024-07-24 11:41:08 -0600

    cambio de interfaz al menu principal

--- ProyectoMicrocrontroladores2/MenuPrincipal.Designer.cs
+++ ProyectoMicrocrontroladores2/MenuPrincipal.Designer.cs
@@ -235,7 +235,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(1032, 577);
+            this.ClientSize = new System.Drawing.Size(1194, 577);
             this.Controls.Add(this.toolStrip1);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.IsMdiContainer = true;
