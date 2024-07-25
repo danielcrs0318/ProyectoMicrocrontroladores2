0d6069ff4b33e451c1a5464d02c6fc6fd8e56ab0
ronhacker01 <ronalsalatielh@gmail.com>
2024-07-25 13:18:45 -0600

    agregue char

--- ProyectoMicrocrontroladores2/formularios/frmSensorMQ-7.Designer.cs
+++ ProyectoMicrocrontroladores2/formularios/frmSensorMQ-7.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSensorMQ_7));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.BTNDESCONECTAR = new System.Windows.Forms.Button();
             this.BTNCONECTAR = new System.Windows.Forms.Button();
@@ -38,8 +41,10 @@
             this.dgvMonoxido = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMonoxido)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnGuardar
@@ -145,12 +150,29 @@
             this.label1.TabIndex = 11;
             this.label1.Text = "SENSOR DE MONOXIDO DE CARBONO(CO)";
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(696, 77);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(389, 519);
+            this.chart1.TabIndex = 19;
+            this.chart1.Text = "chart1";
+            // 
             // frmSensorMQ_7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(955, 651);
+            this.ClientSize = new System.Drawing.Size(1106, 651);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.BTNDESCONECTAR);
             this.Controls.Add(this.BTNCONECTAR);
@@ -165,6 +187,7 @@
             this.Load += new System.EventHandler(this.frmSensorMQ_7_Load);
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvMonoxido)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,5 +204,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dgvMonoxido;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
