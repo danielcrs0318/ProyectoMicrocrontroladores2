36b2643a2d2634447b6a6513b45e2e39c6e95614
ronhacker01 <ronalsalatielh@gmail.com>
2024-07-25 22:46:50 -0600

    Agrege Clase DataCO

---
+++ ProyectoMicrocrontroladores2/Clases/ClaseDataCO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoMicrocrontroladores2.Clases
+{
+    public class ClaseDataCO
+    {
+        private DataTable listaSensorMQ7;
+
+
+        public ClaseDataCO()
+        {
+            listaSensorMQ7 = new DataTable();
+            ClaseConexion c = new ClaseConexion();
+            string sql = $"select sh.IDMQ7 as IDsensor,sh.NivelSensor as NivelCO,sh.Estado as Estado, sh.fecha as FECHA from SensorMQ7 as sh ";
+            listaSensorMQ7 = c.Query(sql);
+        }
+
+        
+        public DataTable ListaSensorMQ7
+        {
+            get { return listaSensorMQ7; }
+        }
+    }
+    /*internal class ClaseDataCO
+    {
+    }*/
+}
