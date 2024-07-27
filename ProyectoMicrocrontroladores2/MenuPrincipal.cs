91046794272b0d08ffc381cfc5189550abaa533c
ronhacker01 <ronalsalatielh@gmail.com>
2024-07-27 14:27:47 -0600

    cambios menu mq7

--- ProyectoMicrocrontroladores2/MenuPrincipal.cs
+++ ProyectoMicrocrontroladores2/MenuPrincipal.cs
@@ -44,14 +44,14 @@ namespace ProyectoMicrocrontroladores2
 
         private void toolStripButton2_Click(object sender, EventArgs e)
         {
-
+            frmSensorMQ_7 frmSensorMQ_7 = new frmSensorMQ_7();
+            frmSensorMQ_7.MdiParent = this;
+            frmSensorMQ_7.Show();
         }
 
         private void toolStripButton4_Click(object sender, EventArgs e)
         {
-            frmSensorMQ_7 frmSensorMQ_7 = new frmSensorMQ_7();
-            frmSensorMQ_7.MdiParent = this;
-            frmSensorMQ_7.Show();
+            
         }
 
         private void toolStripButton3_Click(object sender, EventArgs e)
